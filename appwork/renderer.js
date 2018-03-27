0126ae21de091a8cbee51c1010690f0645df7ba6
avyukd <avyukd@gmail.com>
2018-03-27 14:39:36 -0400

    sample table created on click

--- appwork/renderer.js
+++ appwork/renderer.js
@@ -4,7 +4,19 @@ function sendForm(event) {
     event.preventDefault() // stop the form from submitting
     let numd = document.getElementById("numd").value;
     ipcRenderer.send('form-submission', numd)
-	document.getElementById("mainhead").innerHTML = numd;
+	var parent = document.getElementById("center");
+	var child = document.getElementById("classform");
+	parent.removeChild(child);
+	document.getElementById("mainhead").innerHTML = "Step 3 - Copy to your custom file structure";
+	document.getElementById("secondheader").innerHTML = "Copy content from your machine onto our portable USB drive seamlessly";
+	
+	var table = document.getElementById("classtable");
+	var row = table.insertRow(0);
+	var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+cell1.innerHTML = "NEW CELL1";
+cell2.innerHTML = "NEW CELL2";
+	parent.appendChild(para);
 }
 
 
